a932fe25d9a109a6f31e7518e39e137f6910cea1
vophanlee <vophanlee@gmail.com>
2022-12-25 17:42:53 +0800

    feat: use lazy.nvim instead of paq

--- lua/deps.lua
+++ lua/deps.lua
@@ -1,21 +1,33 @@
-require "paq" {
-    "neovim/nvim-lspconfig";          -- Mind the semi-colons
-    "folke/tokyonight.nvim";          -- theme
-    "projekt0n/github-nvim-theme";    -- github theme
-    "nvim-lualine/lualine.nvim";      -- status line
-    "nvim-treesitter/nvim-treesitter";
-    "nvim-lua/plenary.nvim";          -- telescope dependences
-    {"nvim-telescope/telescope.nvim", branch="0.1.x"};
-    'windwp/nvim-autopairs';
-    -- auto complete
-    'hrsh7th/cmp-nvim-lsp';
-    'hrsh7th/cmp-buffer';
-    'hrsh7th/cmp-path';
-    'hrsh7th/cmp-cmdline';
-    'hrsh7th/nvim-cmp';
-    'L3MON4D3/LuaSnip';
-    'saadparwaiz1/cmp_luasnip';
-    ----------------
-    'nvim-tree/nvim-web-devicons'; -- explorer icon
-    'nvim-tree/nvim-tree.lua';
-}
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "--single-branch",
+    "https://github.com/folke/lazy.nvim.git",
+    lazypath,
+  })
+end
+vim.opt.runtimepath:prepend(lazypath)
+
+require("lazy").setup({
+  "neovim/nvim-lspconfig"; -- Mind the semi-colons
+  "folke/tokyonight.nvim"; -- theme
+  "nvim-lualine/lualine.nvim"; -- status line
+  "nvim-treesitter/nvim-treesitter";
+  "nvim-lua/plenary.nvim"; -- telescope dependences
+  { "nvim-telescope/telescope.nvim", branch = "0.1.x" };
+  'windwp/nvim-autopairs';
+  -- auto complete
+  'hrsh7th/cmp-nvim-lsp';
+  'hrsh7th/cmp-buffer';
+  'hrsh7th/cmp-path';
+  'hrsh7th/cmp-cmdline';
+  'hrsh7th/nvim-cmp';
+  'L3MON4D3/LuaSnip';
+  'saadparwaiz1/cmp_luasnip';
+  ----------------
+  'nvim-tree/nvim-web-devicons'; -- explorer icon
+  'nvim-tree/nvim-tree.lua';
+})

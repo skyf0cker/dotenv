91012bb9c0af12715427d15bac142446f0e3943c
vophanlee <vophanlee@gmail.com>
2022-12-25 16:15:47 +0800

    initial commit

---
+++ lua/deps.lua
@@ -0,0 +1,21 @@
+require "paq" {
+    "neovim/nvim-lspconfig";          -- Mind the semi-colons
+    "folke/tokyonight.nvim";          -- theme
+    "projekt0n/github-nvim-theme";    -- github theme
+    "nvim-lualine/lualine.nvim";      -- status line
+    "nvim-treesitter/nvim-treesitter";
+    "nvim-lua/plenary.nvim";          -- telescope dependences
+    {"nvim-telescope/telescope.nvim", branch="0.1.x"};
+    'windwp/nvim-autopairs';
+    -- auto complete
+    'hrsh7th/cmp-nvim-lsp';
+    'hrsh7th/cmp-buffer';
+    'hrsh7th/cmp-path';
+    'hrsh7th/cmp-cmdline';
+    'hrsh7th/nvim-cmp';
+    'L3MON4D3/LuaSnip';
+    'saadparwaiz1/cmp_luasnip';
+    ----------------
+    'nvim-tree/nvim-web-devicons'; -- explorer icon
+    'nvim-tree/nvim-tree.lua';
+}
---
+++ lua/telescope-extension.lua
@@ -0,0 +1,39 @@
+require('telescope').setup{
+  defaults = {
+    -- Default configuration for telescope goes here:
+    -- config_key = value,
+    mappings = {
+      i = {
+        -- map actions.which_key to <C-h> (default: <C-/>)
+        -- actions.which_key shows the mappings for your picker,
+        -- e.g. git_{create, delete, ...}_branch for the git_branches picker
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+    find_files = {
+      -- hidden = true,
+    }
+    -- Default configuration for builtin pickers goes here:
+    -- picker_name = {
+    --   picker_config_key = value,
+    --   ...
+    -- }
+    -- Now the picker_config_key will be applied every time you call this
+    -- builtin picker
+  },
+}
+
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- lsp picker key mapping
+vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})
+vim.keymap.set('n', 'gr', builtin.lsp_references, {})
+vim.keymap.set('n', 'gi', builtin.lsp_implementations, {})
+vim.keymap.set('n', 'gss', builtin.git_status, {})
+

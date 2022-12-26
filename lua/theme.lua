c47fd51404ae6428ad4009dae4d38ed97cc044ba
lihao68 <lihao68@meituan.com>
2022-12-26 11:14:44 +0800

    feat: add bold property into function & add gitignore

--- lua/theme.lua
+++ lua/theme.lua
@@ -10,7 +10,7 @@ require("tokyonight").setup({
     -- Value is any valid attr-list value for `:help nvim_set_hl`
     comments = { italic = true },
     keywords = { italic = true },
-    functions = {},
+    functions = { bold = true },
     variables = {},
     -- Background styles. Can be "dark", "transparent" or "normal"
     sidebars = "dark", -- style for sidebars, see below

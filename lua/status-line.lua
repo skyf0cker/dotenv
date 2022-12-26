f45a778a8a7f02fb43c26401a528a54dbd3e1fd7
lihao68 <lihao68@meituan.com>
2022-12-26 21:02:27 +0800

    feat: change lualine filename & add telescope treesitter keymap

--- lua/status-line.lua
+++ lua/status-line.lua
@@ -1,5 +1,16 @@
 require('lualine').setup({
   options = {
     theme = 'tokyonight'
+  },
+  sections = {
+    lualine_c = {
+      {
+        'filename',
+        file_status = true, -- Displays file status (readonly status, modified status)
+        path = 3,
+
+        shorting_target = 40 -- Shortens path to leave 40 spaces in the window
+      }
+    }
   }
 })
--- lua/telescope-extension.lua
+++ lua/telescope-extension.lua
@@ -33,6 +33,8 @@ vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+vim.keymap.set('n', '<leader>ft', builtin.treesitter, {})
+
 
 -- lsp picker key mapping
 vim.keymap.set('n', 'gd', builtin.lsp_definitions, {})

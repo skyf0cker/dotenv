ee70b55a22cc8658ca1fb4bb68926f02e7c394f9
lihao68 <lihao68@meituan.com>
2023-01-05 12:43:14 +0800

    alpha dashboard

---
+++ lua/dashboard.lua
@@ -0,0 +1,21 @@
+--[[
+]]
+
+local dashboard = require('alpha.themes.dashboard')
+
+dashboard.section.header.val = {
+    ".------..------..------..------..------..------.",
+    "|V.--. ||O.--. ||P.--. ||H.--. ||A.--. ||N.--. |",
+    "| :(): || :/\\: || :/\\: || :/\\: || (\\/) || :(): |",
+    "| ()() || :\\/: || (__) || (__) || :\\/: || ()() |",
+    "| '--'V|| '--'O|| '--'P|| '--'H|| '--'A|| '--'N|",
+    "`------'`------'`------'`------'`------'`------'",
+}
+
+dashboard.section.buttons.val = {
+    dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
+    dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
+    dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+    dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
+    dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+}
--- lua/deps.lua
+++ lua/deps.lua
@@ -1,48 +1,54 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "--single-branch",
-    "https://github.com/folke/lazy.nvim.git",
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "--single-branch",
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
+    })
 end
 vim.opt.runtimepath:prepend(lazypath)
 
 require("lazy").setup({
-  "neovim/nvim-lspconfig", -- Mind the semi-colons
-  "folke/tokyonight.nvim", -- theme
-  "nvim-lualine/lualine.nvim", -- status line
-  "nvim-treesitter/nvim-treesitter",
-  "nvim-lua/plenary.nvim", -- telescope dependences
-  { "nvim-telescope/telescope.nvim", branch = "0.1.x" },
-  'windwp/nvim-autopairs',
-  -- auto complete
-  'hrsh7th/cmp-nvim-lsp',
-  'hrsh7th/cmp-buffer',
-  'hrsh7th/cmp-path',
-  'hrsh7th/cmp-cmdline',
-  'hrsh7th/nvim-cmp',
-  'L3MON4D3/LuaSnip',
-  'saadparwaiz1/cmp_luasnip',
-  ----------------
-  'nvim-tree/nvim-web-devicons', -- explorer icon
-  'nvim-tree/nvim-tree.lua',
-  'numToStr/Comment.nvim',
-  'akinsho/toggleterm.nvim',
-  {
-    'jedrzejboczar/toggletasks.nvim',
-    dependences = {
-      'nvim-lua/plenary.nvim',
-      'akinsho/toggleterm.nvim',
-      'nvim-telescope/telescope.nvim',
+    "neovim/nvim-lspconfig", -- Mind the semi-colons
+    "folke/tokyonight.nvim", -- theme
+    "nvim-lualine/lualine.nvim", -- status line
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-lua/plenary.nvim", -- telescope dependences
+    { "nvim-telescope/telescope.nvim", branch = "0.1.x" },
+    'windwp/nvim-autopairs',
+    -- auto complete
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'hrsh7th/cmp-cmdline',
+    'hrsh7th/nvim-cmp',
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+    ----------------
+    'nvim-tree/nvim-web-devicons', -- explorer icon
+    'nvim-tree/nvim-tree.lua',
+    'numToStr/Comment.nvim',
+    'akinsho/toggleterm.nvim',
+    {
+        'jedrzejboczar/toggletasks.nvim',
+        dependences = {
+            'nvim-lua/plenary.nvim',
+            'akinsho/toggleterm.nvim',
+            'nvim-telescope/telescope.nvim',
+        },
     },
-  },
-  {
-    "loctvl842/breadcrumb.nvim",
-    dependences = { "nvim-tree/nvim-web-devicons" }
-  },
-  'lewis6991/gitsigns.nvim',
+    {
+        "loctvl842/breadcrumb.nvim",
+        dependences = { "nvim-tree/nvim-web-devicons" }
+    },
+    'lewis6991/gitsigns.nvim',
+    {
+        'goolord/alpha-nvim',
+        config = function()
+            require 'alpha'.setup(require 'alpha.themes.dashboard'.config)
+        end,
+    }
 })
